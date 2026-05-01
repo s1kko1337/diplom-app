386d90a767481ebf5c443ea111efb8cca135910b
s1kko1337 <r.melo.kk@gmail.com>
2026-05-01 17:04:29 +0300

    test(e2e): EquipmentDetailPage и EquipmentDashboardPage POM

---
+++ tests/e2e/pages/EquipmentDetailPage.js
@@ -0,0 +1,22 @@
+export class EquipmentDetailPage {
+  constructor(page, id) {
+    this.page = page
+    this.id = id
+  }
+
+  async goto() {
+    await this.page.goto(`/equipment/${this.id}`)
+  }
+
+  tab(name) {
+    return this.page.getByRole('tab', { name })
+  }
+
+  async openTab(name) {
+    await this.tab(name).click()
+  }
+
+  get sensorsTable() {
+    return this.page.getByRole('table')
+  }
+}
