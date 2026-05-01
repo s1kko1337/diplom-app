736593c21d0655433d1ef39b196c4f44040c288a
s1kko1337 <r.melo.kk@gmail.com>
2026-05-01 16:58:46 +0300

    test(e2e): пресеты viewport

---
+++ tests/e2e/helpers/viewport.js
@@ -0,0 +1,6 @@
+export const VIEWPORTS = {
+  tablet: { width: 768, height: 1024 },
+  laptop: { width: 1440, height: 900 },
+  fullhd: { width: 1920, height: 1080 },
+  mobile: { width: 390, height: 844 },
+}
